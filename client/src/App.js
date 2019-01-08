72bef0f35a66591559f5783c742eaec4941a48c4
Steve Roche <1steveroche@gmail.com>
2019-01-08 07:30:08 +0530

    Removed semicolon

--- client/src/App.js
+++ client/src/App.js
@@ -84,8 +84,8 @@ class Searchbar extends Component {
   onSubmit = () => {
     fetch(`/getStudent/${this.state.search}`)
       .then(res => res.json())
-	  .then(data => this.props.searchCallback(data));
-	  .catch(err => console.log(err));
+      .then(data => this.props.searchCallback(data))
+      .catch(err => console.log(err));
   };
 
   render() {
